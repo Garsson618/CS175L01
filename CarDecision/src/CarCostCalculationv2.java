50c460b6279787910c1f6672e9827f1e2d84002c
Brandon Garsson <s1293728@monmouth.edu>
2020-02-10 13:03:44 -0500

    Carv2

---
+++ CarDecision/src/CarCostCalculationv2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+import java.lang.Number;
+public class CarCostCalculationv2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner in = new Scanner(System.in);
+		
+		System.out.print("Enter information for the regular car: ");
+		String Reg1 = in.nextLine();
+		String nameReg = Reg1.substring(0,14);
+		String costRegStr = Reg1.substring(15,20);
+		double costReg = Double.parseDouble(costRegStr);
+		String mpgRegStr = Reg1.substring(21,23);
+		double mpgReg = Double.parseDouble(mpgRegStr);
+		
+		System.out.print("Enter information for the hyrbrid car: ");
+		String Hybrid1 = in.nextLine();
+		String nameHybrid = Hybrid1.substring(0,12);
+		String costHybridStr = Hybrid1.substring(13,18);
+		double costHybrid = Double.parseDouble(costHybridStr);
+		String mpgHybridStr = Hybrid1.substring(19,21);
+		double mpgHybrid = Double.parseDouble(mpgHybridStr);
+	
+		System.out.print("Enter miles traveled in a year: ");
+		double milesPerYear = in.nextDouble();
+		System.out.print("Enter cost per gallon of gas: ");
+		double costGas = in.nextDouble();
+		double Reg = costGas*(milesPerYear/mpgReg);
+		double Hybrid = costGas*(milesPerYear/mpgHybrid);
+		System.out.println("Cost to own Toyota Corolla after year 1 for regular car: " + (costReg+Reg) + " for Toyota Prius: " + (costHybrid+Hybrid));
+		System.out.println("Cost to own Toyota Corolla after year 2 for regular car: " + (costReg+2*Reg) + " for Toyota Prius: " + (costHybrid+2*Hybrid));
+		System.out.println("Cost to own Toyota Corolla after year 3 for regular car: " + (costReg+3*Reg) + " for Toyota Prius: " + (costHybrid+3*Hybrid));
+		System.out.println("Cost to own Toyota Corolla after year 4 for regular car: " + (costReg+4*Reg) + " for Toyota Prius: " + (costHybrid+4*Hybrid));
+		System.out.println("Cost to own Toyota Corolla after year 5 for regular car: " + (costReg+5*Reg) + " for Toyota Prius: " + (costHybrid+5*Hybrid));
+		double Reg5 = costReg+5*Reg;
+		double Hybrid5 = costHybrid+5*Hybrid;
+		
+		if (Reg5 > Hybrid5)
+			System.out.println("The Toyota Prius pays back after 5 years");
+		else
+			System.out.println("The Toyota Corolla pays back after 5 years");
+		
+	}
+
+}
